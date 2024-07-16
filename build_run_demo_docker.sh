53c0be4b2cf57b885e90646cd86acea7a5e59fc8
Hugo Miomandre <hugo.miomandre@insa-rennes.fr>
2024-07-16 22:03:36 +0200

    updated build/run script to only rebuild image if necessary

--- build_run_demo_docker.sh
+++ build_run_demo_docker.sh
@@ -7,6 +7,13 @@ test -e /dev/video2 && CAM2=--device=/dev/video2:/dev/video2
 test -e /dev/video3 && CAM3=--device=/dev/video3:/dev/video3
 echo $CAM0 $CAM1 $CAM2 $CAM3
 
-docker build . -t genetic_car
+IMAGE_NAME="genetic_car"
+
+# Test if image exists
+docker image inspect $IMAGE_NAME > /dev/null
+
+if [ $? -ne 0 ]; then
+    docker build . -t $IMAGE_NAME
+fi
 xhost +local:docker
-docker run --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY $CAM0 $CAM1 $CAM2 $CAM3 --rm -it genetic_car
+docker run --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY $CAM0 $CAM1 $CAM2 $CAM3 --rm -it $IMAGE_NAME
--- build_run_demo_docker_backup.sh
+++ build_run_demo_docker_backup.sh
@@ -7,6 +7,13 @@ test -e /dev/video2 && CAM2=--device=/dev/video2:/dev/video2
 test -e /dev/video3 && CAM3=--device=/dev/video3:/dev/video3
 echo $CAM0 $CAM1 $CAM2 $CAM3
 
-docker build . -t genetic_car_backup -f backup.Dockerfile
+IMAGE_NAME="genetic_car_backup"
+
+# Test if image exists
+docker image inspect $IMAGE_NAME > /dev/null
+
+if [ $? -ne 0 ]; then
+    docker build . -t $IMAGE_NAME -f backup.Dockerfile
+fi
 xhost +local:docker
-docker run --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY $CAM0 $CAM1 $CAM2 $CAM3 --rm -it genetic_car_backup
+docker run --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY $CAM0 $CAM1 $CAM2 $CAM3 --rm -it $IMAGE_NAME
